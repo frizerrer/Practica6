505a017b18e699709db6eb0465562f0f0c6c87a8
frize <frizerre@gmail.com>
2024-04-29 06:20:41 +0300

    Добавлены сообщения для ввода и вывода

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,10 +6,10 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Введите значение параметра x = ");
             double x = Convert.ToDouble(Console.ReadLine());
             double F = Math.Sin(x) - 2 * Math.Cos(x);
-            Console.WriteLine($" {F} {x}");
+            Console.WriteLine($"F = {F} при x = {x}");
         }
     }
 }
