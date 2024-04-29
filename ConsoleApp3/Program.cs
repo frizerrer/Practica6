00e2ef171a0052373763eb5926a9a65f59fee844
frize <frizerre@gmail.com>
2024-04-29 06:16:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double F = Math.Sin(x) - 2 * Math.Cos(x);
+            Console.WriteLine($"{x} {F}");
+        }
+    }
+}
